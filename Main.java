7b285251f06b043683e447bdd15eb3eed2597f5e
DaN1C3 <danayswrya@gmail.com>
2023-07-04 20:46:29 +0800

    newest

--- Main.java
+++ Main.java
@@ -2,43 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Mobil {
-    private String merk;
-    private String warna;
-    private boolean tersedia;
-
-    public Mobil(String merk, String warna) {
-        this.merk = merk;
-        this.warna = warna;
-        this.tersedia = true;
-    }
-
-    public String getMerk() {
-        return merk;
-    }
-
-    public String getWarna() {
-        return warna;
-    }
-
-    public boolean isTersedia() {
-        return tersedia;
-    }
-
-    public void setTersedia(boolean tersedia) {
-        this.tersedia = tersedia;
-    }
-
-    @Override
-    public String toString() {
-        return "|MOBIL |" +
-                "MERK = '" + merk + '\'' +
-                "| WARNA = '" + warna + '\'' +
-                "| TERSEDIA = " + tersedia +
-                '|';
-    }
-}
-
 class RentalMobil {
     private List<Mobil> mobilList;
     private Scanner scanner;
